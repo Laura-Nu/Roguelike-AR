a6d90043c1ebd6c310f65feda5550eb2cbc737b8
Harold Ramirez <rph1010522@est.univalle.edu>
2024-09-03 08:36:22 -0400

    Tienda e Inventario (Incompleto)

---
+++ Assets/01_Scripts/ItemSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Item_", menuName ="ScriptableObjects/Item")]
+public class ItemSO : ScriptableObject
+{
+    public string itemName;
+    [TextArea] public string description;
+    public Sprite sprite;}
---
+++ Assets/01_Scripts/Shop.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public ItemSO item;
+
+    void OnMouseDown()
+    {
+        //Compra de un item
+        //InventoryManager.Instance.AddItem(item);
+    }
+}
